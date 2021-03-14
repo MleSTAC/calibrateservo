0fa192b4b65dac651245d65590e62a652bfa0aa5
MleSTAC <78829273+MleSTAC@users.noreply.github.com>
2021-03-15 09:05:54 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(90)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(69)
+    servos.P0.run(50)
 })
 basic.showLeds(`
     # . . . .
